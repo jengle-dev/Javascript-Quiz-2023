780a830f8f0ac9e795a7459183de320ab958134d
jengle-dev <j.engle.dev@gmail.com>
2023-02-05 18:59:06 -0600

    updated script and added questions js from initial project because they are my questions

---
+++ Assets/script.js
@@ -0,0 +1,29 @@
+// Global Variables
+var quizRules = document.getElementById('quizRules');
+
+// Buttons Global Variables
+var startButton = document.getElementById('startButton');
+var exitQuizBtn = document.getElementById('exitBtn');
+var restartQuizBtn = document.getElementById('restartBtn');
+var nextQuestionBtn = document.getElementById('nextBtn');
+
+// Card Global Variables & Elements
+var questionCard = document.getElementById('questionContainer');
+var quizQuestionText = document.getElementById('questionText');
+var currentQuestionNumber = document.getElementById('questionNumber');
+
+// Answer Buttons
+var answerButtonA = document.getElementById('answer-A');
+var answerButtonB = document.getElementById('answer-B');
+var answerButtonC = document.getElementById('answer-C');
+var answerButtonD = document.getElementById('answer-D');
+
+// Timer
+
+
+
+
+
+function refreshPage() {
+    window.location.reload();
+}
