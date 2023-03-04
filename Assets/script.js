3149ffccaf88e743b5b57030662ec21a41052c5c
jengle-dev <j.engle.dev@gmail.com>
2023-03-04 01:07:15 -0600

    fresh code. npm install. tracking of referenced sites. Bootstrap getting used.

--- Assets/script.js
+++ Assets/script.js
@@ -1,73 +1,14 @@
-// Global Variables
-var quizRules = document.getElementById('quizRules');
 
-// Buttons Global Variables
-var startButton = document.getElementById('startButton');
-var exitQuizBtn = document.getElementById('exitBtn');
-var restartQuizBtn = document.getElementById('restartBtn');
-var nextQuestionBtn = document.getElementById('nextBtn');
+var mainPage = document.getElementById("mainLoad");
+var quizStart = document.getElementById("startBtn");
+var quizCard = document.getElementById("questionCard");
+// start button - add event listener
+// at click of start button - timer begins
+// when answer chosen, the next question appears
+// display question number of total questions
+// if answer chosen is incorrect, time is deducted from the timer as a penalty
+// quiz is over when timer reaches 0 or all questions are answered
+// score is saved, initials and score
+// view high scores
+// retake the quiz
 
-// Card Global Variables & Elements
-var questionCard = document.getElementById('questionContainer');
-var quizQuestionText = document.getElementById('questionText');
-var currentQuestionNumber = document.getElementById('questionNumber');
-
-// Answer Buttons
-var answerButtonA = document.getElementById('answer-A');
-var answerButtonB = document.getElementById('answer-B');
-var answerButtonC = document.getElementById('answer-C');
-var answerButtonD = document.getElementById('answer-D');
-
-// Timer
-// If the timer count down is finished, write some text // used https://codepen.io/TLJens/pen/azedap to help with the logic of the timer countdown
-var interval;
-// Timer function
-function countdownTime() {
-    clearInterval(interval);
-    interval = setInterval(function () {
-        var timer = $('#timerCountdown').html();
-        timer = timer.split(':');
-        var minutes = timer[0];
-        var seconds = timer[1];
-        seconds -= 1;
-        if (minutes < 0) return;
-        else if (seconds < 0 && minutes != 0) {
-            minutes -= 1;
-            seconds = 59;
-        }
-        else if (seconds < 10 && length.seconds != 2) seconds = '0' + seconds;
-
-        $('#timerCountdown').html(minutes + ':' + seconds);
-
-        if (minutes == 0 && seconds == 0) clearInterval(interval);
-    }, 1000);
-}
-
-$('.startTimer').click(function () {
-    $('#timerCountdown').text("15:00");
-    countdownTime();
-    startQuiz();
-});
-
-// $('#js-resetTimer').click(function () {
-//     $('#timerCountdown').text("15:00");
-//     clearInterval(interval);
-// });
-
-// If user answers incorrectly, subtract 5 seconds from timer
-
-// Hide instructions upon startBtn click and display timer and question 1 etc.
-function startQuiz() {
-    quizRules.classList.add('hide');
-    startButton.classList.add('hide');
-    var quizTimer = document.getElementById('timerDisplay');
-    quizTimer.classList.remove('hide');
-}
-
-// Answer Buttons - Listen for clicks    
-// event.stopPropagation(); //prevents any click running all functions
-
-
-function refreshPage() {
-    window.location.reload();
-}
