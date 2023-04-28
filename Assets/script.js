4c8b64da67a3616ce7091c34be3006ec7fa39933
jengle-dev <j.engle.dev@gmail.com>
2023-04-28 18:53:26 -0500

    updated script.js with tutor. need to fix click propagation for buttons and function to get shuffle and next question etc.

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,3 @@
-
 var mainPage = document.getElementById("mainLoad");
 var quizStart = document.getElementById("startBtn");
 var quizCard = document.getElementById("questionCard");
@@ -8,19 +7,6 @@ var timer = document.getElementsByClassName("timer");
 
 // invoke shuffled questions function
 let shuffledQuestions, currentQuestionIndex;
-
-// start button - add event listener
-quizStart = addEventListener("click", // directions hide and question card displays
-    function startQuiz() {
-
-        console.log("Quiz started.")
-
-        mainPage.classList.add('hide');
-        quizCard.classList.remove('hide');
-
-        setNextQuestion();
-        countdown();
-    });
 
 // at click of start button - timer begins
 // countdown function from Algorithms Folder in Module 4
@@ -37,44 +23,30 @@ setNextQuestion = () => {
 
     // randomize questions from questions.js array
     displayQuestion = (questions) => {
-        shuffledQuestions = questions.sort(() => Math.random() - .5);
+        shuffledQuestions = questions.sort(() => Math.random() - 0.5);
         currentQuestionIndex = 0;
-    }
+    };
 
     // document.set
     // .innerText = questions[id].question;
-}
-displayQuestion(shuffledQuestions[currentQuestionIndex]);
-
+};
+// displayQuestion(shuffledQuestions[currentQuestionIndex]);
 
 resetState = () => {
-    // needs to reset the page 
+    // needs to reset the page
     // make it so click event can happen again on the next question
-}
-
+};
 
 // display question number of total questions and/or progress bar
 
 // answer options from buttons
-const answer1 = document.getElementById('btnOne');
-const answer2 = document.getElementById('btnTwo');
-const answer3 = document.getElementById('btnThree');
-const answer4 = document.getElementById('btnFour');
+const answer1 = document.getElementById("btnOne");
+const answer2 = document.getElementById("btnTwo");
+const answer3 = document.getElementById("btnThree");
+const answer4 = document.getElementById("btnFour");
 
 // adding text of questions from questions array
-questionText.innerText = questions[id].question;
-
-// adding text answers from questions array
-btnOne.innerText = questions[id].answers[0].text;
-btnTwo.innerText = questions[id].answers[1].text;
-btnThree.innerText = questions[id].answers[2].text;
-btnFour.innerText = questions[id].answers[3].text;
-// if answer chosen is incorrect, time is deducted from the timer as a penalty
-
-
-// score is saved, initials and score
-// view high scores
-// retake the quiz
+// questionText.innerText = questions[id].question;
 
 // creating an array and passing the number, questions, options, and answers
 const questions = [
@@ -87,7 +59,7 @@ const questions = [
             { text: "Hyper Text Markup Language", correct: true },
             { text: "Hyper Text Multiple Language", correct: false },
             { text: "Hyper Tool Multi Language", correct: false },
-        ]
+        ],
     },
     {
         // numb: 2, index: 1
@@ -97,8 +69,8 @@ const questions = [
             { text: "Common Style Sheet", correct: false },
             { text: "Colorful Style Sheet", correct: false },
             { text: "Computer Style Sheet", correct: false },
-            { text: "Cascading Style Sheet", correct: true }
-        ]
+            { text: "Cascading Style Sheet", correct: true },
+        ],
     },
     {
         // numb: 3, index: 2
@@ -109,51 +81,71 @@ const questions = [
             { text: "===", correct: true },
             { text: "==", correct: false },
             { text: "!=", correct: false },
-        ]
+        ],
     },
     {
         // numb: 4, index: 3
         id: 4,
         question: "What does a 'for' loop instruct the code to do?",
         answers: [
-            { text: "Run a condition a specified number of times within an array.", correct: true },
-            { text: "The 'for' loop contains the condition outside of the parentheses.", correct: false },
-            { text: "Describe to the console what the condition is 'for'.", correct: false },
+            {
+                text: "Run a condition a specified number of times within an array.",
+                correct: true,
+            },
+            {
+                text: "The 'for' loop contains the condition outside of the parentheses.",
+                correct: false,
+            },
+            {
+                text: "Describe to the console what the condition is 'for'.",
+                correct: false,
+            },
             { text: "Structured Query Language", correct: false },
-        ]
+        ],
     },
     {
         // numb: 5, index: 4
         id: 5,
-        question: "Which of the following objects is the main entry point to all client-side JavaScript features and APIs?",
+        question:
+            "Which of the following objects is the main entry point to all client-side JavaScript features and APIs?",
         answers: [
             { text: "Window", correct: true },
             { text: "Position", correct: false },
             { text: "Document", correct: false },
             { text: "Location", correct: false },
-        ]
+        ],
     },
     {
         // numb: 6, index: 5
         id: 6,
-        question: "Arrays in JavaScript are defined by which of the following statements?",
+        question:
+            "Arrays in JavaScript are defined by which of the following statements?",
         answers: [
             { text: "It is an ordered list of string.", correct: false },
             { text: "It is an ordered list of functions.", correct: false },
             { text: "It is an ordered list of objects.", correct: false },
             { text: "It is an ordered list of values.", correct: true },
-        ]
+        ],
     },
     {
         // numb: 7, index: 6
         id: 7,
         question: "What is JavaScript?",
         answers: [
-            { text: "JavaScript is a scripting language used to make the website interactive.", correct: true },
-            { text: "JavaScript is a compiled language used to make the website interactive.", correct: false },
-            { text: "JavaScript is an assembly language used to make the website interactive.", correct: false },
+            {
+                text: "JavaScript is a scripting language used to make the website interactive.",
+                correct: true,
+            },
+            {
+                text: "JavaScript is a compiled language used to make the website interactive.",
+                correct: false,
+            },
+            {
+                text: "JavaScript is an assembly language used to make the website interactive.",
+                correct: false,
+            },
             { text: "None of the mentioned.", correct: false },
-        ]
+        ],
     },
     {
         // numb: 8, index: 7
@@ -164,7 +156,7 @@ const questions = [
             { text: "JavaScript is an Object-Based language.", correct: true },
             { text: "JavaScript is an Object-Oriented language.", correct: false },
             { text: "JavaScript is a High-level language.", correct: false },
-        ]
+        ],
     },
     {
         // numb: 9, index: 8
@@ -175,17 +167,50 @@ const questions = [
             { text: "Null type.", correct: false },
             { text: "Undefined type.", correct: false },
             { text: "Number type.", correct: false },
-        ]
+        ],
     },
     {
         // numb: 10, index: 9
         id: 10,
-        question: "Which of the following can be used to call a JavaScript Code Snippet?",
+        question:
+            "Which of the following can be used to call a JavaScript Code Snippet?",
         answers: [
             { text: "Preprocessor", correct: false },
             { text: "Function/Method", correct: true },
             { text: "Triggering Event", correct: false },
             { text: "RMI", correct: false },
-        ]
+        ],
     },
 ];
+
+function renderQuestion(event) {
+    event.stopPropagation(); //buttons are globally being listened to
+    const currentQuestion = 1;
+    quizQuestion.textContent = questions[currentQuestion].question;
+
+    // adding text answers from questions array
+    btnOne.innerText = questions[currentQuestion].answers[0].text;
+    btnTwo.innerText = questions[currentQuestion].answers[1].text;
+    btnThree.innerText = questions[currentQuestion].answers[2].text;
+    btnFour.innerText = questions[currentQuestion].answers[3].text;
+    // if answer chosen is incorrect, time is deducted from the timer as a penalty
+
+    // score is saved, initials and score
+    // view high scores
+    // retake the quiz
+};
+
+// start button - add event listener
+quizStart = addEventListener(
+    "click", // directions hide and question card displays
+    function startQuiz() {
+        console.log("Quiz started.");
+
+        mainPage.classList.add("hide");
+        quizCard.classList.remove("hide");
+        renderQuestion();
+        setNextQuestion();
+        countdown();
+    }
+);
+
